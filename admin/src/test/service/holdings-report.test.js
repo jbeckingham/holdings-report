41eb5e7dacbb9674feceb2331252b0c781501501
jbeckingham <jabeckingham@gmail.com>
2022-01-27 15:03:50 +0000

    Clean up and complete set of tests for holdings report service. Add companies service url to config

--- admin/src/test/service/holdings-report.test.js
+++ admin/src/test/service/holdings-report.test.js
@@ -12,6 +12,32 @@ const companyMap = {
   2: "The Small Investment Company",
   3: "Capital Investments",
 }
+const reportData = [
+  {
+    userId: "1",
+    firstName: "Billy",
+    lastName: "Bob",
+    date: "2020-01-01",
+    holding: "The Small Investment Company",
+    value: 1400,
+  },
+  {
+    userId: "2",
+    firstName: "Sheila",
+    lastName: "Aussie",
+    date: "2020-01-01",
+    holding: "The Big Investment Company",
+    value: 10000,
+  },
+  {
+    userId: "2",
+    firstName: "Sheila",
+    lastName: "Aussie",
+    date: "2020-01-01",
+    holding: "The Small Investment Company",
+    value: 10000,
+  },
+]
 const csv = `"User","First Name","Last Name","Date","Holding","Value"
 "1","Billy","Bob","2020-01-01","The Small Investment Company",1400
 "2","Sheila","Aussie","2020-01-01","The Big Investment Company",10000
@@ -32,47 +58,27 @@ describe("buildLineData", () => {
       investments[0]["holdings"][0],
       companyMap,
     )
-    const expectedResult = {
-      userId: "1",
-      firstName: "Billy",
-      lastName: "Bob",
-      date: "2020-01-01",
-      holding: "The Small Investment Company",
-      value: 1400,
-    }
-    expect(result).toEqual(expectedResult)
+    expect(result).toEqual(reportData[0])
   })
 })
 
 describe("buildReportData", () => {
   test("Builds report data", () => {
     const result = buildReportData(investments, companies)
-    const expectedResult = [
-      {
-        userId: "1",
-        firstName: "Billy",
-        lastName: "Bob",
-        date: "2020-01-01",
-        holding: "The Small Investment Company",
-        value: 1400,
-      },
-      {
-        userId: "2",
-        firstName: "Sheila",
-        lastName: "Aussie",
-        date: "2020-01-01",
-        holding: "The Big Investment Company",
-        value: 10000,
-      },
-      {
-        userId: "2",
-        firstName: "Sheila",
-        lastName: "Aussie",
-        date: "2020-01-01",
-        holding: "The Small Investment Company",
-        value: 10000,
-      },
-    ]
-    expect(result).toEqual(expectedResult)
+    expect(result).toEqual(reportData)
+  })
+})
+
+describe("convertToCsv", () => {
+  test("Converts report to csv", () => {
+    const result = convertToCsv(reportData)
+    expect(result).toEqual(csv)
+  })
+})
+
+describe("generate", () => {
+  test("Generates csv report", () => {
+    const result = convertToCsv(investments, companies)
+    expect(result).toEqual(csv)
   })
 })
